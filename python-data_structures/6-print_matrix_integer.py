ef1c3cf8c0e137fa99d91a0cce4926f55c0aeb75
Essie-wanjiru001 <e.wanjiru@alustudent.com>
2022-11-02 18:12:42 -0700

    printing a matrix of intergers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
+
+        '''
+        matrix = [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ]
+
+        print_matrix_integer(matrix)
+        '''
