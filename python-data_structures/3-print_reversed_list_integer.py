7f2d5d1f0852f78b099631c0c72fbdfb2b615057
Essie-wanjiru001 <e.wanjiru@alustudent.com>
2022-11-02 17:48:51 -0700

    printing intergers in reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_interger(my_List=[]):
-    if my_list:
-        for i in my_list[::-1]:
-            print("{:d}".format(i))
+    if my_list is not None:
+        for i in my_list((len(my_list) - 1), -1,-1):
+            print("{:d}".format(my_List[i]))
+        else:
+            return
